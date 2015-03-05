ad46af1cefff9fb76e511a64d76050436db5ef12
damo.tamas <damo.tamas@aktan.fr>
2015-03-05 16:24:08 +0100

    fixed jshint

--- app/scripts/directives/highlighted.js
+++ app/scripts/directives/highlighted.js
@@ -10,7 +10,7 @@ angular.module('plumber').directive('highlighted', ['$timeout', function($timeou
         post: function(scope, element, attributes) {
           var $element = $(element);
           $timeout(function(){
-            Prism.highlightElement($element.find('code')[0])
+            Prism.highlightElement($element.find('code')[0]);
           }, 0);
         }
       };
